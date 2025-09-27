56821ae31edb90f9310e5dcaad7b7ada7329e505
aapolipponen <aapo.lipponen@gmail.com>
2025-09-27 13:45:44 +0300

    Remove unused character scene and scripts; add new character body implementation

--- characterBody.gd
+++ characterBody.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@export var friction := 0.01
 var target = position
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	var desired_velocity = input_direction * speed
+	velocity = velocity.move_toward(desired_velocity, speed * friction)
 	target = get_global_mouse_position()
 
 func _physics_process(delta):
